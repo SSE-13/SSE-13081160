0ad511084e3ee04c72942b3a5d03dd00ad79b1aa
JohnUU <940646623@qq.com>
2016-04-07 21:43:49 +0800

    改错了

--- animation/renderer.ts
+++ animation/renderer.ts
@@ -1,212 +1,137 @@
 var canvas: HTMLCanvasElement = document.getElementById("game") as HTMLCanvasElement;
 var context = canvas.getContext("2d");
+/**
+ * 基类，负责处理x,y,rotation 等属性
+ */
+class DisplayObject {
 
+    x = 0;
 
-module render {
+    y = 0;
 
+    rotation = 0;
 
-    /**
-     * 基类，负责处理x,y,rotation 等属性
-     */
-    export class DisplayObject {
+    draw(context: CanvasRenderingContext2D) {
+        context.save();
+        context.rotate(this.rotation);
+        context.translate(this.x, this.y);
+        this.render(context);
 
-        x = 0;
-        y = 0;
-        scaleX = 1;
-        scaleY = 1;
-        rotation = 0;
-
-        /**
-         * 全局矩阵
-         */
-        globalMatrix: render.Matrix;
-
-        parent: DisplayObject;
-
-        constructor() {
-            this.globalMatrix = new render.Matrix();
-        }
-
-        draw(context: CanvasRenderingContext2D) {
-
-            var parent = this.parent;
-            var angle = this.rotation / 180 * Math.PI;
-            var skewX = angle;
-            var skewY = angle;
-
-            var localMatrix = new render.Matrix();
-            localMatrix.updateFromDisplayObject(this.x, this.y, this.scaleX, this.scaleY, this.rotation);
-
-            if (!parent) {
-                this.globalMatrix = localMatrix;
-            }
-            else {
-                //TODO:
-                // GLOBAL_MATRIX = PARENT_GLOBAL_MATRIX * LOCAL_MATRIX
-                this.globalMatrix = matrixcount(localMatrix,parent.globalMatrix);
-            }
-
-
-            context.setTransform(
-                this.globalMatrix.a,
-                this.globalMatrix.b,
-                this.globalMatrix.c,
-                this.globalMatrix.d,
-                this.globalMatrix.tx,
-                this.globalMatrix.ty
-            );
-            this.render(context);
-        }
-
-        render(context: CanvasRenderingContext2D) {
-
-        }
+        context.restore();
     }
-    
-    function  matrixcount(ma:Matrix,mb:Matrix):Matrix {
-        
-        var mc = new Matrix() ;
-        mc.a = ma.a * mb.a + ma.b * mb.c ;
-        mc.b = ma.a * mb.b + ma.b * mb.d ;
-        mc.c = mb.c * ma.d + mb.a * ma.c ;
-        mc.d = mb.b * ma.c + ma.d * mb.d ;
-        mc.tx = mb.a * ma.tx + mb.c * ma.ty + mb.tx ;
-        mc.ty = mb.d * ma.ty +  mb.b * ma.tx+ mb.ty ;
-        
-        return mc;
+
+    render(context: CanvasRenderingContext2D) {
 
     }
 
-    export class DisplayObjectContainer extends DisplayObject {
+}
+
+class Bitmap extends DisplayObject {
 
 
-        children: Array<DisplayObject>
+    source;
 
-        constructor() {
-            super();
-            this.children = [];
+    render(context: CanvasRenderingContext2D) {
+
+        var image = imagePool[this.source];
+        if (image) {
+            context.drawImage(image, 0, 0);
         }
-
-        addChild(child: DisplayObject) {
-            this.children.push(child);
-            child.parent = this;
-        }
-
-        render(context) {
-            for (var i = 0; i < this.children.length; i++) {
-                var child = this.children[i];
-                child.draw(context);
-            }
-        }
-    }
-
-    export class Bitmap extends DisplayObject {
-
-
-        source;
-
-        render(context: CanvasRenderingContext2D) {
-
-            var image = imagePool[this.source];
-            if (image) {
-                context.drawImage(image, 0, 0);
-            }
-            else {
-                context.font = "20px Arial";
-                context.fillStyle = '#000000';
-                context.fillText('错误的URL', 0, 20);
-            }
-        }
-
-    }
-
-    class Rect extends DisplayObject {
-
-        width = 100
-
-        height = 100;
-
-        color = '#FF0000';
-
-        render(context: CanvasRenderingContext2D) {
-            context.fillStyle = this.color;
-            context.fillRect(0, 0, this.width, this.height);
-        }
-    }
-
-    class TextField extends DisplayObject {
-
-        render(context: CanvasRenderingContext2D) {
+        else {
             context.font = "20px Arial";
             context.fillStyle = '#000000';
-            context.fillText('HelloWorld', 0, 20);
+            context.fillText('错误的URL', 0, 20);
         }
     }
 
+}
 
+class Rect extends DisplayObject {
 
-    var imagePool = {};
+    width = 100
 
-    function loadResource(imageList, callback) {
-        var count = 0;
-        if (imageList.length == 0) {
-            callback();
-            return;
-        }
-        imageList.forEach(function(imageUrl) {
-            var image = new Image();
-            image.src = imageUrl;
-            image.onload = onLoadComplete;
-            image.onerror = onLoadError;
+    height = 100;
 
-            function onLoadComplete() {
-                imagePool[imageUrl] = image;
-                count++;
-                if (count == imageList.length) {
-                    callback();
-                }
-            }
+    color = '#FF0000';
 
-            function onLoadError() {
-                alert('资源加载失败:' + imageUrl);
-            }
-        })
+    render(context: CanvasRenderingContext2D) {
+        context.fillStyle = this.color;
+        context.fillRect(0, 0, this.width, this.height);
     }
+}
+
+class TextField extends DisplayObject {
+
+    render(context: CanvasRenderingContext2D) {
+        context.font = "20px Arial";
+        context.fillStyle = '#000000';
+        context.fillText('HelloWorld', 0, 20);
+    }
+}
 
 
 
+var imagePool = {};
+
+function loadResource(imageList, callback) {
+    var count = 0;
+    if (imageList.length == 0) {
+        callback();
+        return;
+    }
+    imageList.forEach(function(imageUrl) {
+        var image = new Image();
+        image.src = imageUrl;
+        image.onload = onLoadComplete;
+        image.onerror = onLoadError;
+
+        function onLoadComplete() {
+            imagePool[imageUrl] = image;
+            count++;
+            if (count == imageList.length) {
+                callback();
+            }
+        }
+
+        function onLoadError() {
+            alert('资源加载失败:' + imageUrl);
+        }
+    })
+}
+
+
+
+/**
+ * 渲染核心
+ */
+class RenderCore {
+
+    renderQueue;
     /**
-     * 渲染核心
+     * 启动渲染核心
+     * @param renderQueue 渲染队列
+     * @param imageList 资源列表
      */
-    export class RenderCore {
-
-        stage;
-        /**
-         * 启动渲染核心
-         * @param renderQueue 渲染队列
-         * @param imageList 资源列表
-         */
-        start(stage: DisplayObject, resourceList = []) {
-            stage.parent = null;
-            this.stage = stage;
-            var self = this;
-            loadResource(resourceList, function() {
-                requestAnimationFrame(self.onEnterFrame.bind(self));
-            })
-
-        }
-
-        onEnterFrame() {
-            context.save();
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            this.drawQueue(this.stage);
-            context.restore();
-            requestAnimationFrame(this.onEnterFrame.bind(this));
-        }
-
-        drawQueue(stage: DisplayObject) {
-            stage.draw(context);
-        }
+    start(renderQueue = [], resourceList = []) {
+        this.renderQueue = renderQueue;
+        var self = this;
+        loadResource(resourceList, function() {
+            requestAnimationFrame(self.onEnterFrame.bind(self));
+        })
 
     }
+
+    onEnterFrame() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        this.drawQueue(this.renderQueue);
+        requestAnimationFrame(this.onEnterFrame.bind(this));
+    }
+
+    drawQueue(queue) {
+        for (var i = 0; i < this.renderQueue.length; i++) {
+            var displayObject: DisplayObject = this.renderQueue[i];
+            displayObject.draw(context);
+        }
+    }
+
 }
